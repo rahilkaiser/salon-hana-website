0664786c782c7d4303f84ef58744e32748f55d9a
ESX1234 <ESX1234@DESKTOP-KUP00MC>
2024-05-26 05:24:05 +0200

    Added Beauty Info Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import {Hero} from "@/components/sections/hero/Hero";
 import {NextUIProvider} from "@nextui-org/react";
 import {StickyNavbar} from "@/components/core/navbar/StickyNavbar";
-import {Link} from "@nextui-org/link";
 import {Intro} from "@/components/sections/intro/Intro";
+import {BeautySection} from "@/components/sections/beauty-section/BeautySection";
 
 export default function Home() {
     return (
@@ -13,6 +13,7 @@ export default function Home() {
                 <StickyNavbar/>
                 <Hero/>
                 <Intro/>
+                <BeautySection/>
                 {/*<Beauty Programme für Gesicht und Körper mit videopopup/>*/}
                 {/*<Behandlungen/>*/}
                 {/*<Kundenmeinungen/>*/}
