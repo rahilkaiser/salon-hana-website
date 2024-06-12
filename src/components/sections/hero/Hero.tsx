58423630e7b07d00f04f472ef7d2c2d2a7a7e619
ESX1234 <ESX1234@DESKTOP-KUP00MC>
2024-06-12 12:20:21 +0200

    removed Parallax Component

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
 import {HeroText} from "@/components/sections/hero/components/HeroText";
-import Parallax from "@/components/core/parallax/Parallax";
 
 export const Hero: React.FC = () => {
     return (
         <section className="relative h-screen flex items-center justify-center overflow-hidden">
             <div className="absolute inset-0 rounded-lg">
-                <Parallax>
                     <Image
                         src="/images/flipped-bg.jpg"
                         alt="Beauty Salon"
@@ -15,7 +13,6 @@ export const Hero: React.FC = () => {
                         objectFit="cover"  // Ensures the image covers the div completely
                         priority={true}  // Ensures the image is preloaded on the server
                     />
-                </Parallax>
 
             </div>
             <div className="absolute inset-0 bg-black opacity-60 rounded-lg"></div>
